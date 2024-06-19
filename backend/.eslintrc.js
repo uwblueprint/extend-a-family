b3497850cfd73928ece5d75bdf684bcdf1aa2504
Aathithan Chandrabalan <a69chand@uwaterloo.ca>
2024-06-18 20:13:11 -0400

    Revert file to match main

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -8,27 +8,16 @@ module.exports = {
     createDefaultProgram: true,
     tsconfigRootDir: __dirname,
   },
-  settings: {
-    react: {
-      version: "detect",
-    },
-    "import/resolver": {
-      node: {
-        extensions: [".ts", ".tsx", ".native.js"],
-      },
-    },
-  },
   extends: [
     "airbnb-typescript/base",
     "prettier",
     "plugin:prettier/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:import/recommended",
   ],
   rules: {
     "prettier/prettier": ["error", { endOfLine: "auto" }],
-    "import/no-unresolved": [2, { devDependencies: true }],
+    "import/prefer-default-export": "off",
   },
   ignorePatterns: ["build/*"],
 };
