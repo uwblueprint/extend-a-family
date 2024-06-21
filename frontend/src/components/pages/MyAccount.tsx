d21079a1f713a25510c6c6b231c0facb0e2b7216
Jimmy Liu <jimmyjcl753@gmail.com>
2024-06-20 22:23:32 -0400

    Run linter
    Resolve merge conflicts in App.tsx

--- frontend/src/components/pages/MyAccount.tsx
+++ frontend/src/components/pages/MyAccount.tsx
@@ -2,18 +2,19 @@ import React, { useContext } from "react";
 import MainPageButton from "../common/MainPageButton";
 import AuthContext from "../../contexts/AuthContext";
 
-
 const MyAccount = (): React.ReactElement => {
-    const { authenticatedUser } = useContext(AuthContext);
-    return (
-        <div style={{ textAlign: "center", width: "25%", margin: "0px auto" }}>
-            <h1>My Account</h1>
-            <div>First Name: {authenticatedUser?.firstName}</div>
-            <div>Last Name: {authenticatedUser?.lastName}</div>
-            <div style={{ marginBottom: "1rem" }}>Email: {authenticatedUser?.email}</div>
-            <MainPageButton />
-        </div>
-    );
+  const { authenticatedUser } = useContext(AuthContext);
+  return (
+    <div style={{ textAlign: "center", width: "25%", margin: "0px auto" }}>
+      <h1>My Account</h1>
+      <div>First Name: {authenticatedUser?.firstName}</div>
+      <div>Last Name: {authenticatedUser?.lastName}</div>
+      <div style={{ marginBottom: "1rem" }}>
+        Email: {authenticatedUser?.email}
+      </div>
+      <MainPageButton />
+    </div>
+  );
 };
 
 export default MyAccount;
