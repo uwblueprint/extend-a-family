2b15512049a3f0b9e19a27519c027b80a8b4ab87
mxc-maggiechen <mxc.maggiechen@gmail.com>
2024-06-20 21:54:45 -0400

    added richtext, image, textinput, numberinput elements

--- backend/models/courseelement.mgmodel.ts
+++ backend/models/courseelement.mgmodel.ts
@@ -12,7 +12,84 @@ const CourseElementSchema: Schema = new Schema({
   },
 });
 
-export default mongoose.model<CourseElement>(
+export interface RichTextElement extends CourseElement {
+  content: string;
+  backgroundColor?: string;
+}
+
+const RichTextElementSchema: Schema = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  backgroundColor: {
+    type: String,
+    required: false,
+  },
+});
+
+export interface ImageElement extends CourseElement {
+  src: string;
+  alt: string;
+}
+
+const ImageElementSchema: Schema = new Schema({
+  src: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    required: true,
+  },
+});
+
+export interface TextInputElement extends CourseElement {
+  correctAnswer?: string;
+}
+
+const TextInputElementSchema: Schema = new Schema({
+  correctAnswer: {
+    type: String,
+    required: false,
+  },
+});
+
+export interface NumberInputElement extends CourseElement {
+  correctAnswer?: number;
+  prefixText?: string;
+  suffixText?: string;
+}
+
+const NumberInputElementSchema: Schema = new Schema({
+  correctAnswer: {
+    type: Number,
+    required: false,
+  },
+  prefixText: {
+    type: String,
+    required: false,
+  },
+  suffixText: {
+    type: String,
+    required: false,
+  },
+});
+
+const CourseElementModel = mongoose.model<CourseElement>(
   "CourseElement",
   CourseElementSchema,
 );
+
+CourseElementModel.discriminator("RichTextElement", RichTextElementSchema);
+
+CourseElementModel.discriminator("ImageElement", ImageElementSchema);
+
+CourseElementModel.discriminator("TextInputElement", TextInputElementSchema);
+
+CourseElementModel.discriminator(
+  "NumberInputElement",
+  NumberInputElementSchema,
+);
+
+export default CourseElementModel;
