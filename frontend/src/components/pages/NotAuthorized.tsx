396f2667ad56190991eb781bd1d8869b80c6233c
Carolyn Zhang <carolyn.zhang18@gmail.com>
2024-06-22 20:17:28 -0400

    Use escaped apostrophe in NotAuthorized page

--- frontend/src/components/pages/NotAuthorized.tsx
+++ frontend/src/components/pages/NotAuthorized.tsx
@@ -3,8 +3,7 @@ import React from "react";
 const NotAuthorized = (): React.ReactElement => {
   return (
     <div style={{ textAlign: "center" }}>
-      {/* eslint-disable-next-line react/no-unescaped-entities */}
-      <h1>Can't view this page since you don't have permission 🙁</h1>
+      <h1>Can&apos;t view this page since you don&apos;t have permission 🙁</h1>
     </div>
   );
 };
