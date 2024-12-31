2e400946d97e345ee0e8864dd57abaff62d41ce0
Carolyn Zhang <carolyn.zhang18@gmail.com>
2024-12-30 20:12:36 -0500

    Add CourseAuthoringContext to page builder and right sidebar

--- frontend/src/components/pages/CourseAuthoringPage.tsx
+++ frontend/src/components/pages/CourseAuthoringPage.tsx
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box } from "@mui/material";
 import LeftSidebar from "../course_authoring/LeftSidebar";
 import RightSidebar from "../course_authoring/RightSidebar";
 import MainArea from "../course_authoring/MainArea";
+import CourseAuthoringContext from "../../contexts/CourseAuthoringContext";
+import { CourseElement, CoursePage } from "../../types/CourseTypes";
 
 const CourseAuthoringPage = () => {
+  const [activePage, setActivePage] = useState<CoursePage | null>(null);
+  const [activeElement, setActiveElement] = useState<CourseElement | null>(
+    null,
+  );
   return (
-    <Box height="100%" display="flex" flexDirection="column">
-      <Box flexGrow={1} display="flex" flexDirection="row">
-        <Box width="20%">
-          <LeftSidebar />
-        </Box>
-        <Box flexGrow={1}>
-          <MainArea />
-        </Box>
-        <Box width="20%">
-          <RightSidebar />
+    <CourseAuthoringContext.Provider
+      value={{ activePage, setActivePage, activeElement, setActiveElement }}
+    >
+      <Box height="100%" display="flex" flexDirection="column">
+        <Box flexGrow={1} display="flex" flexDirection="row">
+          <Box width="20%">
+            <LeftSidebar />
+          </Box>
+          <Box flexGrow={1}>
+            <MainArea />
+          </Box>
+          <Box width="20%">
+            <RightSidebar />
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </CourseAuthoringContext.Provider>
   );
 };
 
