d36acbcdef7807320e89a3fbef4c30e58aa8f66a
Carolyn Zhang <carolyn.zhang18@gmail.com>
2025-01-02 01:38:02 -0500

    Course page API (+ other course API tweaks)

--- backend/models/coursemodule.mgmodel.ts
+++ backend/models/coursemodule.mgmodel.ts
@@ -28,6 +28,7 @@ export const CourseModuleSchema: Schema = new Schema({
 CourseModuleSchema.set("toObject", {
   virtuals: true,
   versionKey: false,
+  flattenObjectIds: true,
   transform: (_doc: Document, ret: Record<string, unknown>) => {
     // eslint-disable-next-line no-underscore-dangle
     delete ret._id;
