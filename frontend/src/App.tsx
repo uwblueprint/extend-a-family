f60453bb9468d0d5e036e7ddc5116e4dc2ff5256
mxc-maggiechen <mxc.maggiechen@gmail.com>
2025-03-18 01:43:23 -0400

    add back private route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -138,10 +138,11 @@ const App = (): React.ReactElement => {
                   component={HelpRequestPage}
                   allowedRoles={["Facilitator"]}
                 />
-                <Route
+                <PrivateRoute
                   exact
                   path={Routes.COURSE_PAGE}
                   component={CourseViewingPage}
+                  allowedRoles={["Administrator", "Facilitator", "Learner"]}
                 />
                 <PrivateRoute
                   exact
