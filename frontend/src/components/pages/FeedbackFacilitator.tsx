dc22f8fa88cc56fb75e7d74bea5660479146e04f
Jimmy Liu <jimmyjcl753@gmail.com>
2025-11-03 21:36:47 -0500

    Initial commit

--- frontend/src/components/pages/FeedbackFacilitator.tsx
+++ frontend/src/components/pages/FeedbackFacilitator.tsx
@@ -1,11 +1,200 @@
-import React from "react";
+import { ExpandMore, Search } from "@mui/icons-material";
+import {
+  Box,
+  MenuItem,
+  Stack,
+  TextField,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import React, { useState } from "react";
+import { useUser } from "../../hooks/useUser";
+import StartAdornedTextField from "../common/form/StartAdornedTextField";
 
 const FeedbackFacilitator = (): React.ReactElement => {
+  const theme = useTheme();
+  const { role } = useUser();
+  const [isSearchActive, setIsSearchActive] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>Feedback Page</h1>
-      <div style={{ height: "2rem" }} />
-    </div>
+    <Stack
+      direction="column"
+      alignItems="flex-start"
+      justifyContent="space-between"
+      gap="24px"
+      alignSelf="stretch"
+      sx={{ padding: "32px 64px" }}
+    >
+      <Typography variant="headlineLarge" color={theme.palette.Neutral[700]}>
+        View Feedback
+      </Typography>
+      <Stack
+        direction="row"
+        alignItems="flex-start"
+        justifyContent="space-between"
+        gap="32px"
+        alignSelf="stretch"
+      >
+        <StartAdornedTextField
+          variant="outlined"
+          label="Student Name"
+          value={searchQuery}
+          onChange={handleSearch}
+          onFocus={() => setIsSearchActive(true)}
+          onBlur={() => setIsSearchActive(false)}
+          adornment={
+            <Search
+              sx={{
+                color: isSearchActive
+                  ? theme.palette.Neutral[600]
+                  : theme.palette.Neutral[500],
+              }}
+            />
+          }
+          focusedBorderColor={theme.palette[role].Dark.Default}
+          sx={{
+            // minWidth: "400px",
+            flex: "1 0 0",
+            borderRadius: "8px",
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: isSearchActive
+                  ? theme.palette[role].Dark.Default
+                  : theme.palette.Neutral[500],
+              },
+              "&:hover fieldset": {
+                borderColor: theme.palette.Neutral[600],
+              },
+            },
+          }}
+        />
+        <TextField
+          select
+          variant="outlined"
+          label="Unit"
+          sx={{ minWidth: "200px" }}
+        >
+          {["1.0", "2.0", "2.1"].map((roleOption) => (
+            <MenuItem key={roleOption} value={roleOption} onClick={() => {}}>
+              <Typography variant="labelMedium">
+                {roleOption.toUpperCase()}
+              </Typography>
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          select
+          variant="outlined"
+          label="Module"
+          sx={{ minWidth: "200px" }}
+        >
+          {["1.0", "2.0", "2.1"].map((roleOption) => (
+            <MenuItem key={roleOption} value={roleOption} onClick={() => {}}>
+              <Typography variant="labelMedium">
+                {roleOption.toUpperCase()}
+              </Typography>
+            </MenuItem>
+          ))}
+        </TextField>
+      </Stack>
+      <hr
+        style={{
+          border: "1px solid",
+          width: "100%",
+          color: theme.palette.Neutral[300],
+          margin: 0,
+        }}
+      />
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        padding="24px"
+        width="100%"
+        sx={{
+          borderRadius: "8px",
+          border: `1px solid ${theme.palette.Neutral[300]}`,
+          background: theme.palette.Facilitator.Light.Default,
+        }}
+      >
+        <Typography variant="titleMedium" color={theme.palette.Neutral[700]}>
+          Olivia C.
+        </Typography>
+        <Typography variant="titleMedium" color={theme.palette.Neutral[700]}>
+          Unit 1
+        </Typography>
+        <Stack
+          direction="column"
+          gap="24px"
+          paddingBottom="24px"
+          alignItems="flex-start"
+        >
+          <Stack direction="row" gap="64px">
+            <Stack direction="row" gap="16px">
+              <Stack direction="row" gap="8px" alignItems="center">
+                <Box
+                  sx={{
+                    width: "20px",
+                    height: "20px",
+                    borderRadius: "50%",
+                    background: theme.palette.Facilitator.Dark.Default,
+                  }}
+                />
+                <Box
+                  sx={{
+                    width: "20px",
+                    height: "20px",
+                    borderRadius: "50%",
+                    background: theme.palette.Facilitator.Dark.Default,
+                  }}
+                />
+                <Box
+                  sx={{
+                    width: "20px",
+                    height: "20px",
+                    borderRadius: "50%",
+                    background: theme.palette.Neutral[400],
+                  }}
+                />
+                <Box
+                  sx={{
+                    width: "20px",
+                    height: "20px",
+                    borderRadius: "50%",
+                    background: theme.palette.Neutral[400],
+                  }}
+                />
+                <Box
+                  sx={{
+                    width: "20px",
+                    height: "20px",
+                    borderRadius: "50%",
+                    background: theme.palette.Neutral[400],
+                  }}
+                />
+              </Stack>
+              <Typography
+                variant="titleMedium"
+                color={theme.palette.Neutral[600]}
+              >
+                Easy
+              </Typography>
+            </Stack>
+            <Typography
+              variant="titleMedium"
+              color={theme.palette.Neutral[600]}
+            >
+              100% Liked
+            </Typography>
+            <ExpandMore />
+          </Stack>
+        </Stack>
+      </Stack>
+    </Stack>
   );
 };
 
