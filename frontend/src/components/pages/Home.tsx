caad82662c9ce5f9f33d193ad4fc0107ecd7ba46
Nadeen <nadeenfindakly@gmail.com>
2025-11-06 21:03:42 -0500

    Add sidebar to the learner home page

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,20 +1,51 @@
-import React from "react";
+// import React from "react";
 
-const HomeContent = () => {
-  return (
-    <div>
-      <div>Currently under construction 🛠️</div>
-    </div>
-  );
-};
+// const HomeContent = () => {
+//   return (
+//     <div>
+//       <div>Currently under construction 🛠️</div>
+//     </div>
+//   );
+// };
+
+// const Home = (): React.ReactElement => {
+//   return (
+//     <div style={{ textAlign: "center" }}>
+//       <h1>Home Page</h1>
+//       <div style={{ height: "2rem" }} />
+//       <HomeContent />
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import UnitSidebar from "../course_viewing/sidebar/UnitSidebar";
+import { CourseUnit } from "../../types/CourseTypes";
 
 const Home = (): React.ReactElement => {
+  const [selectedUnit, setSelectedUnit] = useState<CourseUnit | null>(null);
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
+  // const handleDrawerOpen = () => {
+  //   setSidebarOpen(true);
+  // };
+
+  const handleDrawerClose = () => {
+    setSidebarOpen(true);
+  };
+
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>Home Page</h1>
-      <div style={{ height: "2rem" }} />
-      <HomeContent />
-    </div>
+    <Box display="flex" width="100%">
+      <UnitSidebar
+        setSelectedUnit={setSelectedUnit}
+        handleClose={handleDrawerClose}
+        open={sidebarOpen}
+        selectedUnit={selectedUnit}
+      />
+    </Box>
   );
 };
 
