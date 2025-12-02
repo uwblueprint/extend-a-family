13f05883ddcc378ebc74c6480e44275ac2f2669a
nadeen-f <109323099+nadeen-f@users.noreply.github.com>
2025-12-01 23:55:44 -0500

    Learner homepage facilitator card (#162)
    * Add facilitator card to home page
    
    * Update learner to have facilitator fields instead of just the object id
    
    * add facilitator props in the facilitator card
    
    * add the facilitator card to the learner home page
    
    * Fix formatting for the facilitator card
    
    * Create and add navigation button components to the learner home page
    
    * restrict facilitator fields so that only the required ones are available to learners
    
    * hide pronouns if the facilitator hasn't added them, and add default bio

---
+++ frontend/src/components/learners/FacilitatorCard.tsx
@@ -0,0 +1,88 @@
+import Avatar from "@mui/material/Avatar";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import { useTheme } from "@mui/material";
+import Typography from "@mui/material/Typography";
+
+type ProfileFieldProps = {
+  label: string;
+  children: React.ReactNode;
+};
+
+function ProfileField({ label, children }: ProfileFieldProps) {
+  return (
+    <Box display="flex" alignItems="flex-start">
+      <Typography
+        variant="labelLarge"
+        sx={{ minWidth: 100, marginRight: 1, paddingTop: "2.5px" }}
+      >
+        {label}:
+      </Typography>
+
+      <Typography variant="bodyMedium">{children}</Typography>
+    </Box>
+  );
+}
+
+type FacilitatorCardProps = {
+  facilitator: {
+    firstName: string;
+    lastName: string;
+    pronouns?: string;
+    email: string;
+    bio?: string;
+    profilePic?: string;
+  };
+};
+
+export default function FacilitatorCard({ facilitator }: FacilitatorCardProps) {
+  const theme = useTheme();
+  const initials =
+    `${facilitator.firstName[0]}${facilitator.lastName[0]}`.toUpperCase();
+  return (
+    <Card
+      variant="outlined"
+      sx={{ width: 1045, height: 230, borderRadius: "10px" }}
+    >
+      <CardContent sx={{ height: "100%" }}>
+        <Box
+          display="flex"
+          flexDirection="row"
+          alignItems="center"
+          gap="50px"
+          height="100%"
+          p="20px"
+        >
+          <Avatar
+            sx={{
+              width: 155,
+              height: 155,
+              bgcolor: theme.palette.Facilitator.Light.Default,
+              color: theme.palette.Facilitator.Dark.Default,
+              ...theme.typography.displayLarge,
+            }}
+            src={facilitator.profilePic}
+          >
+            {!facilitator.profilePic && initials} {/* fallback to initials */}
+          </Avatar>
+          <Stack sx={{ gap: "15px" }}>
+            <ProfileField label="Name">
+              {facilitator.firstName} {facilitator.lastName}
+            </ProfileField>
+            {facilitator.pronouns && (
+              <ProfileField label="Pronouns">
+                {facilitator.pronouns}
+              </ProfileField>
+            )}
+            <ProfileField label="Email"> {facilitator.email} </ProfileField>
+            <ProfileField label="Bio">
+              {facilitator.bio || "It's nice to meet you!"}
+            </ProfileField>
+          </Stack>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+}
